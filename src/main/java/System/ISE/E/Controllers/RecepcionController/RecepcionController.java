2a152d98ca5b9abe1b9324dc9e7e1cdcc7c8f1f9
abrahan24 <asaraviacoca@gmail.com>
2024-07-03 17:59:09 -0400

    update

--- src/main/java/System/ISE/E/Controllers/RecepcionController/RecepcionController.java
+++ src/main/java/System/ISE/E/Controllers/RecepcionController/RecepcionController.java
@@ -2,15 +2,11 @@ package System.ISE.E.Controllers.RecepcionController;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
-import org.springframework.validation.annotation.Validated;
 
 import System.ISE.E.Models.Dao.ClienteDao;
 import System.ISE.E.Models.Dao.DetalleRecepcionDao;
@@ -30,12 +26,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
